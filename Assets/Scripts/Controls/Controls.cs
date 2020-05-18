85b559b0435e316da5bafdd6ffce2ab95fc79ad2
Elliot Koh <appdev.moose@gmail.com>
2020-05-18 13:37:54 +0800

    first commit, with tile placement

---
+++ Assets/Scripts/Controls/Controls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    HexTile lastHovered;
+    void Update()
+    {
+        Ray ray  = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast (ray, out hit);
+        
+        HexTile tile = null;
+        if(hasHit){
+            tile = hit.transform.gameObject.GetComponent<HexTile>();        
+        }
+        
+        if(lastHovered && lastHovered != tile) 
+        {
+            lastHovered.SetHovered(false);
+            lastHovered = null;
+        }
+
+        if(tile && lastHovered != tile) 
+        {
+            tile.SetHovered(true);
+            lastHovered = tile;
+        }
+
+        if(Input.GetMouseButtonDown(0) && tile)
+        {
+            tile.Build(TileType.MINERAL);
+        }
+    }
+}
