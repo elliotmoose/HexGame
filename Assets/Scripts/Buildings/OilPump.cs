8c03e6744cbbeb50f1e887f3afd80615d696b40f
Elliot Koh <appdev.moose@gmail.com>
2020-06-12 01:38:34 +0800

    changed angles

--- Assets/Scripts/Buildings/OilPump.cs
+++ Assets/Scripts/Buildings/OilPump.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class OilPump : Building
 {
+
+    public float oilAmount = 15;
+    public float oilInterval = 1;
+    private float _curOilInterval = 0;
+
     // Start is called before the first frame update
-    void Start()
+    protected override void InitializeResourceNeeds() 
     {
-        
+        SetNeedsResource(ResourceIdentifiers.ENERGY);
+        AddResourceIndicator(ResourceIdentifiers.ENERGY, "This building needs energy to work!");
     }
 
-    // Update is called once per frame
+    public override void BuildingTick() 
+    {
+        if(HasResource(ResourceIdentifiers.ENERGY))
+        {
+            Player.GetInstance().TransactMinerals(oilAmount);
+        }
+    }
+
     protected override void Update()
     {
         base.Update();
+        _curOilInterval += Time.deltaTime;
+        if(_curOilInterval >= oilInterval)
+        {
+            Trigger();
+            _curOilInterval = 0;
+        }
+    }
+
+    void Trigger() 
+    {
     }
 }
